e15c20a343f5350443f4a8b6a5458db893297683
Juliana Suedde <juhsuedde@gmail.com>
2023-09-08 16:40:52 -0300

    adicinoada correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -25,17 +25,31 @@ app.post("/movies", async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } = req.body;
 
     try {
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: {
+                title: { equals: title, mode: "insensitive" }
+            }
+            // aqui o title foi omitido por ter o mesmo nome
+        });
+
+        if (movieWithSameTitle) {
+            return res.status(409)
+                .send({ message: "Já existe um filme com esse título" });
+        }
+
         await prisma.movie.create({
             data: {
                 title: title,
                 genre_id: genre_id,
                 language_id: language_id,
                 oscar_count: oscar_count,
+                // como os de cima têm o mesmo nome, poderiam ser omitidos (apenas "title," por exemplo)
                 release_date: new Date(release_date)
             }
         });
     } catch (error) {
-        return res.status(500).send({ message: "Falha ao cadastrar um filme" });
+        return res.status(500)
+            .send({ message: "Falha ao cadastrar um filme" });
     }
 
     res.status(201).send();
